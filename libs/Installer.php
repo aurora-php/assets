bc61b2563069ff4d91c90e5ae7034611f22b6995
Harald Lapp <harald@octris.org>
2017-06-02 22:18:04 +0200

    work on assets installer

--- libs/Installer.php
+++ libs/Installer.php
@@ -31,38 +31,38 @@ class Installer
     const LOG_INFO = 1;
     const LOG_WARNING = 2;
     const LOG_ERROR = 3;
-    
+
     /**
      * Namespace of extra field of composer.json for asset configuration.
-     * 
+     *
      * @type    string
      */
     const NS_EXTRA = 'octris/assets';
 
     /**
      * Default assets namespace.
-     * 
+     *
      * @type    string
      */
     const NS_ASSETS = 'assets';
 
     /**
      * Instance of composer io channel.
-     * 
+     *
      * @type    \Composer\IO\IOInterface;
      */
     protected $io;
 
     /**
      * Assets directories.
-     * 
+     *
      * @type    array
      */
     protected $assets_dirs = array();
 
     /**
      * Path to root package.
-     * 
+     *
      * @type    string
      */
     protected $root_path;
@@ -84,9 +84,9 @@ class Installer
                                     : array(self::NS_ASSETS => $extra[self::NS_EXTRA]['target']));
         }
 
-        var_dump([$target_dir, $this->assets_dirs]);
+        var_dump([$this->assets_dirs]);
     }
-    
+
     /**
      * Install assets for a package.
      */
@@ -94,7 +94,7 @@ class Installer
     {
         $package = $event->getOperation()->getPackage();
         $extra = $package->getExtra();
-        
+
         if (isset($extra[self::NS_EXTRA]) && isset($extra[self::NS_EXTRA]['source'])) {
             // installed/updated package has assets to install
             $package_path = $event->getComposer()->getInstallationManager()->getInstallPath($package);
@@ -110,22 +110,22 @@ class Installer
                 } elseif (!($return = is_dir($package_path . '/' . $dir))) {
                     $this->log(self::LOG_WARNING, sprintf('%s: asset directory does not exist "%s".', $package_name, $dir));
                 }
-                
+
                 return $return;
             }, ARRAY_FILTER_USE_BOTH);
 
             foreach ($source_dirs as $ns => $dir) {
-                // $target_dir =
-                //
-                // if (!mkdir()
+                $target_path = $this->root_path . '/' . $this->assets_dirs[$ns] . '/' . $package_name;
+
+                print $target_path;
             }
 
             var_dump($source_dirs);
         }
-        
+
         return $this;
     }
-    
+
     /**
      * Log status messages.
      */
