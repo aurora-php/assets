eb0fb52c03432f09880840e9d73d7257ebb8d5b9
Harald Lapp <h.lapp@clipdealer.de>
2017-06-02 14:42:36 +0200

    fixed plugin installer

--- libs/InstallerPlugin.php
+++ libs/InstallerPlugin.php
@@ -12,8 +12,11 @@
 namespace Octris\Assets;
 
 use Composer\Composer;
-use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
+use Composer\IO\IOInterface;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\Script\Event;
+use Composer\Script\ScriptEvents;
 
 /**
  * Assets installer plugin.
@@ -21,11 +24,29 @@ use Composer\Plugin\PluginInterface;
  * @copyright   copyright (c) 2017 by Harald Lapp
  * @author      Harald Lapp <harald@octris.org>
  */
-class InstallerPlugin implements PluginInterface
+class InstallerPlugin implements PluginInterface, EventSubscriberInterface
 {
+    protected $installer;
+    
     public function activate(Composer $composer, IOInterface $io)
     {
-        $installer = new Installer($composer, $io);
-        $composer->getInstallationManager()->addInstaller($installer);
+        $this->installer = new Installer($composer, $io);
+    }
+    
+    public static function getSubscribedEvents()
+    {
+        return array(
+            ScriptEvents::POST_UPDATE_CMD => array(
+                array('onPostInstall', 0)
+            ),
+            ScriptEvents::POST_INSTALL_CMD => array(
+                array('onPostInstall', 0)
+            )
+        );
+    }
+
+    public function onPostInstall(Event $event)
+    {
+        $this->installer->install();
     }
 }
