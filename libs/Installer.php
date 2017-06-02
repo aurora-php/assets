a04179ae05fc8853bfe291f9f4d28dfc31d26c75
Harald Lapp <h.lapp@clipdealer.de>
2017-06-02 17:28:37 +0200

    work on assets installer

--- libs/Installer.php
+++ libs/Installer.php
@@ -12,9 +12,10 @@
 namespace Octris\Assets;
 
 use Composer\Composer;
-use Composer\Json\JsonFile;
 use Composer\IO\IOInterface;
 use Composer\Package;
+use Composer\Package\PackageInterface;
+use Composer\Installer\PackageEvent;
 
 /**
  * Assets installer.
@@ -25,48 +26,121 @@ use Composer\Package;
 class Installer
 {
     /**
+     * Log levels.
+     */
+    const LOG_INFO = 1;
+    const LOG_WARNING = 2;
+    const LOG_ERROR = 3;
+    
+    /**
      * Namespace of extra field of composer.json for asset configuration.
+     * 
+     * @type    string
      */
     const NS_EXTRA = 'octris/assets';
 
     /**
      * Default assets namespace.
+     * 
+     * @type    string
      */
     const NS_ASSETS = 'assets';
 
     /**
-     * Instance of composer.
+     * Instance of composer io channel.
+     * 
+     * @type    \Composer\IO\IOInterface;
      */
-    protected $composer;
+    protected $io;
 
     /**
      * Assets directories.
+     * 
+     * @type    array
      */
     protected $assets_dirs = array();
 
     /**
+     * Path to root package.
+     * 
+     * @type    string
+     */
+    protected $root_path;
+
+    /**
      * Constructor.
      */
-    public function __construct($composer, $io)
+    public function __construct(Composer $composer, IOInterface $io)
     {
-        $this->composer = $composer;
-        $this->package = $composer->getPackage();
-        $extra = $this->package->getExtra();
+        $this->io = $io;
+        $this->root_path = realpath(dirname(\Composer\Factory::getComposerFile()));
+
+        $package = $composer->getPackage();
+        $extra = $package->getExtra();
 
         if (isset($extra[self::NS_EXTRA]) && isset($extra[self::NS_EXTRA]['target'])) {
-            $this->assets_dir = (is_array($extra[self::NS_EXTRA]['target'])
+            $this->assets_dirs = (is_array($extra[self::NS_EXTRA]['target'])
                                     ? $extra[self::NS_EXTRA]['target']
                                     : array(self::NS_ASSETS => $extra[self::NS_EXTRA]['target']));
         }
-        
-        var_dump($this->assets_dir);
+
+        var_dump([$target_dir, $this->assets_dirs]);
     }
     
     /**
-     * Install assets.
+     * Install assets for a package.
      */
-    public function install()
+    public function install(PackageEvent $event)
     {
+        $package = $event->getOperation()->getPackage();
+        $extra = $package->getExtra();
+        
+        if (isset($extra[self::NS_EXTRA]) && isset($extra[self::NS_EXTRA]['source'])) {
+            // installed/updated package has assets to install
+            $package_path = $event->getComposer()->getInstallationManager()->getInstallPath($package);
+            $package_name = $package->getName();
+
+            $source_dirs = (is_array($extra[self::NS_EXTRA]['source'])
+                                ? $extra[self::NS_EXTRA]['source']
+                                : array(self::NS_ASSETS => $extra[self::NS_EXTRA]['source']));
+
+            $source_dirs = array_filter($source_dirs, function($dir, $ns) use ($package_path) {
+                if (!($return = isset($this->assets_dirs[$ns]))) {
+                    $this->log(self::LOG_WARNING, sprintf('%s: namespace not defined in root package "%s".', $package_name, $ns));
+                } elseif (!($return = is_dir($package_path . '/' . $dir))) {
+                    $this->log(self::LOG_WARNING, sprintf('%s: asset directory does not exist "%s".', $package_name, $dir));
+                }
+                
+                return $return;
+            }, ARRAY_FILTER_USE_BOTH);
+
+            foreach ($source_dirs as $ns => $dir) {
+                // $target_dir =
+                //
+                // if (!mkdir()
+            }
+
+            var_dump($source_dirs);
+        }
+        
         return $this;
+    }
+    
+    /**
+     * Log status messages.
+     */
+    protected function log($type, $message)
+    {
+        switch ($type) {
+            case self::LOG_INFO:
+                $this->io->write(array('<info>' . $message . '</info>'));
+                break;
+            case self::LOG_WARNING:
+                $this->io->write(array('<warning>' . $message . '</warning>'));
+                break;
+            case self::LOG_ERROR:
+                $this->io->write(array('<error>' . $message . '</error>'));
+                break;
+        }
     }
 }
